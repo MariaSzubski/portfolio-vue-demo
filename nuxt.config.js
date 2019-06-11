c3eb9f02341868e4d5c24effa82ffdbb05433fec
Maria Szubski <hello@mariaszubski.com>
2019-06-10 20:00:48 -0400

    Add favicons

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,16 +8,41 @@ export default {
   */
   head: {
     title: "Maria Szubski Digital, LLC • Web Development Consultant • Freelance Front End Developer",
+    htmlAttrs: {
+      lang: 'en',
+    },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { hid: 'description', name: 'description', content: pkg.description },
+      { content: '#f3695a', name: 'msapplication-TileColor' },
+      { content: '#ffffff', name: 'theme-color' }
     ],
     link: [
-      {
+      { // Start Favicon
         rel: 'icon',
-        type: 'image/x-icon', href: '/favicon.ico'
+        type: 'image/x-icon', href: '/favicon/favicon.ico'
       },
+      {
+        rel: 'icon', sizes: '180x180',
+        type: 'image/png', href: '/favicon/apple-touch-icon.png'
+      },
+      {
+        rel: 'icon', sizes: '32x32',
+        type: 'image/png', href: '/favicon/favicon-32x32.png'
+      },
+      {
+        rel: 'icon', sizes: '16x16',
+        type: 'image/png', href: '/favicon/favicon-16x16.png'
+      },
+      {
+        rel: 'manifest', sizes: '16x16',
+        href: '/favicon/site.webmanifest'
+      },
+      {
+        rel: 'mask-icon', sizes: 'color: #f3695a',
+        href: '/favicon/safari-pinned-tab.svg'
+      }, // End Favicon
       {
         // Default Font
         rel: 'stylesheet',

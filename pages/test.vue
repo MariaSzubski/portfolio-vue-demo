6332960f0994afee0fac6e1af8b229c197780e30
Maria Szubski <hello@mariaszubski.com>
2019-06-24 02:02:34 -0400

    Remove outdated Project styles

--- pages/test.vue
+++ pages/test.vue
@@ -15,13 +15,11 @@
 
 <script>
 import ProjectCard from '~/components/ProjectCard.vue'
-import ProjectCard6 from '~/components/ProjectCard6.vue'
 import projects from '~/data/all-projects.yml'
 
 export default {
   components: {
-    ProjectCard,
-    ProjectCard6
+    ProjectCard
   },
   data(){
     return { passProjects: projects }
